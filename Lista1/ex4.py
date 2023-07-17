2283ce87ec0df0ef4dc1f5bcb1c745bda5d3f9fa
JulioHerculano <julyocesarplay@gmail.com>
2023-07-17 19:27:16 -0300

    Exercicio 4 terminado

---
+++ Lista1/ex4.py
@@ -0,0 +1,6 @@
+print("Insira suas 4 notas bimestrais: ")
+nota1 = input(); nota2 = input(); nota3 = input(); nota4 = input()
+media = (float(nota1) + float(nota2) + float( nota3) + float(nota4)) /4
+print("Sua média é: ", media)
+
+
