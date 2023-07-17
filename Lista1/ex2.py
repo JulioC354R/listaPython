d66dbf1c0745ab31a831e28cd40184af7f2a9bc4
JulioHerculano <julyocesarplay@gmail.com>
2023-07-17 19:13:56 -0300

    exercícios, 1, 2 e 3 feitos

---
+++ Lista1/ex2.py
@@ -0,0 +1,2 @@
+valor = input("Insira um valor")
+print ("O valor informado foi ", valor)
---
+++ Lista1/ex3.py
@@ -0,0 +1,4 @@
+valor1 = input("insira o primeiro número: ")
+valor2 = input("insira o segundo número: ")
+soma = int(valor1) + int(valor2)
+print ("A soma dos dois números é: " , soma)
